a54c26b34421334e8c4a52f635c60ef1456cf5cf
Adam Davis <adavis@thesdg.com>
2020-05-04 21:40:00 -0400

    feat: Add initial spring-di code

--- spring-di/src/main/java/com/apress/spring_quick/di/App.java
+++ spring-di/src/main/java/com/apress/spring_quick/di/App.java
@@ -3,12 +3,39 @@
  */
 package com.apress.spring_quick.di;
 
+import com.apress.spring_quick.config.AppSpringConfig;
+import com.apress.spring_quick.di.model.Message;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        final ApplicationContext applicationContext;
+        final String type = args.length == 0 ? "annotation" : args[0];
+        switch (type) {
+            case "xml":
+                applicationContext =
+                        new ClassPathXmlApplicationContext("classpath:/application.xml");
+                break;
+            case "annotation":
+                applicationContext =
+                        new AnnotationConfigApplicationContext(AppSpringConfig.class);
+                break;
+            default:
+                throw new IllegalArgumentException(type + " unknown");
+        }
+        final MyBeanInterface myBean = applicationContext.getBean(MyBeanInterface.class);
+
+        System.out.println("Ran using " + type + " type Spring config");
+        System.out.println(myBean.getComment());
+
+        final MessageRepository messageRepository = applicationContext.getBean(MessageRepository.class);
+        messageRepository.save(new Message("My first message"));
+        messageRepository.save(new Message("My second message"));
+
+        System.out.println("All Messages = " + messageRepository.findAll());
     }
+
 }
